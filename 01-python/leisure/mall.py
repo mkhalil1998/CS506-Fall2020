bebe7304f5fc752a68ed66f28be89f2f1cd4f0e4
mkhalil1998 <70872915+mkhalil1998@users.noreply.github.com>
2020-09-13 23:27:38 -0400

    Created my mall and commiting it

--- 01-python/leisure/mall.py
+++ 01-python/leisure/mall.py
@@ -1,3 +1,14 @@
-def draw_mall():
-    print("mall not found")
-    return
+def draw_mall(height=2):
+    print("----------------------------------------------")
+    print("|               CS_506 MALL                  |")     
+    print("|                                            |")
+    for i in range(height):
+        print("|                                            |")
+    print("|                                            |")     
+    print("|   -------                      ---------   |")     
+    print("|  |  H&M  |                    |   ZARA  |  |")     
+    print("|  |       |     __________     |         |  |")  
+    print("|  |       |    |     |    |    |         |  |") 
+    print("|  |       |    |     |    |    |         |  |")
+    print("----------------------------------------------")
+    return 
